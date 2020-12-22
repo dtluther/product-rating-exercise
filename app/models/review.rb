849a8b77c64f9f678a36ebcb6be4374f1aea5f9c
Dillon Luther <dillon.luther@gmail.com>
2020-12-22 14:28:27 -0700

    fixing scope

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,5 +6,5 @@ class Review < ApplicationRecord
 
   belongs_to :product
 
-  default_scope { order(created_at: :desc, rating:, :desc) }
+  default_scope { order(created_at: :desc, rating: :desc) }
 end
